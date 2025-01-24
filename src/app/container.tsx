ba99e47db9c5294a1053ab2dfeecd0e5cbdbffb9
AndrewBlairDCT <andrew.blair@dcthomson.co.uk>
2025-01-24 16:57:26 +0000

    check answer puts song object in state

---
+++ src/app/container.tsx
@@ -0,0 +1,67 @@
+'use client'
+
+import { useState } from 'react';
+
+interface Song {
+    title: string,
+    artist: string,
+    chart_position: number,
+    year_released: number
+}
+
+interface JsonData {
+    data: {
+    songs: [
+        song: Song
+    ]
+    }
+}
+
+const Container: React.FC<JsonData> = ({data}) => {
+    const [currentSong, setCurrentSong] = useState({});
+    const [previousSong, setPreviousSong] = useState({});
+    const [userAnswer, setUserAnswer] = useState("");
+   
+    function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+        setUserAnswer(e.target.value);
+      }
+    
+    const checkAnswer = (e: React.FormEvent) => {
+        e.preventDefault();
+        if (data.songs.find(song => song.title === userAnswer)) {
+            const userSong = data.songs.filter(song => song.title === userAnswer)
+            console.log(typeof userSong)
+            setCurrentSong(userSong)
+        } else {
+            return false
+        }
+    }
+
+    console.log(currentSong)
+    
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      {/* <h1>Song: {data.songs[0].title}</h1> */}
+      <form onSubmit={checkAnswer}>
+          <input
+            className="border-2 border-indigo-600 p-2"
+            name="userAnswer"
+            value={userAnswer}
+            onChange={handleChange}
+            placeholder="Enter Song title..."
+          />
+          <button
+            className="border border-indigo-600 m-2 p-2"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      </main>
+      
+    </div>
+  );
+}
+
+export default Container;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,13 @@
+import Container from './container';
 import { promises as fs } from 'fs';
 
 export default async function Home() {
   const file = await fs.readFile(process.cwd() + '/src/app/songs.json', 'utf8');
   const data = JSON.parse(file);
-  console.log(data.songs[0])
+  
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-      <h1>{data.songs[0].title}</h1>
-      </main>
-      
-    </div>
+   <Container 
+   data={data}/>
   );
 }
