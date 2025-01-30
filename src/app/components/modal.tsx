810d23c291e8b8330290f0f45221ecbadb9cc2dd
AndrewBlairDCT <andrew.blair@dcthomson.co.uk>
2025-01-30 11:26:58 +0000

    added modal

---
+++ src/app/components/modal.tsx
@@ -0,0 +1,26 @@
+interface modalProps {
+    setModalOpen: (filterTerm: boolean) => void;
+  }
+  
+  const Modal: React.FC<modalProps> = ({setModalOpen}) => {
+    return (
+        <div className="rounded-md bg-red-950 p-2 text-white justify-center fixed z-10 h-100 overflow-y-auto">
+        <div className="flex-col items-end justify-center p-4 text-center sm:items-center sm:p-0">
+        <h3 className="font-bold text-lg">How to Play</h3>
+        <p className="py-4">First, correctly name a song written by Gary Barlow.</p>
+        <p className="py-4">Then name another song written by Gary Barlow that had higher or equal position in the UK singles chart.</p>
+        <p className="py-4">You are allowed three wrong answers, then it's game over.</p>
+        <button className="rounded-md bg-indigo-600 p-2"
+        onClick={() => setModalOpen(false)}>
+        Close</button>
+        </div>
+        </div>
+    
+     
+        
+       
+        
+    )
+};
+
+export default Modal;
--- src/app/container.tsx
+++ src/app/container.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react';
+import Modal from './components/modal';
 
 interface Song {
     title: string,
@@ -19,21 +20,30 @@ const Container: React.FC<JsonData> = ({items}) => {
     const [currentSong, setCurrentSong] = useState({});
     const [previousSong, setPreviousSong] = useState({});
     const [userAnswer, setUserAnswer] = useState("");
+    const [isCorrect, setIsCorrect] = useState(false);
+    const [isIncorrect, setIsIncorrect] = useState(false);
+    const [livesLeft, setLivesLeft] = useState(3);
+    const [modalOpen, setModalOpen] = useState(false)
+
    
     function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
         setUserAnswer(e.target.value);
+        
       }
     
-      console.log(items)
+  
 
     const checkAnswer = (e: React.FormEvent) => {
         e.preventDefault();
         if (items.find(song => song.title === userAnswer)) {
+            setIsCorrect(true)
             const userSong = items.filter(song => song.title === userAnswer)
-            console.log(typeof userSong)
             setCurrentSong(userSong)
+            setUserAnswer('')
         } else {
-            return false
+          setIsIncorrect(true)
+          setLivesLeft(livesLeft - 1)
+          setUserAnswer('')
         }
     }
 
@@ -41,23 +51,44 @@ const Container: React.FC<JsonData> = ({items}) => {
     
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h1>Setting the Gary Bar Low</h1>
+      <button className="bg-indigo-600 text-white rounded-md border border-indigo-600 m-2 p-2"
+      onClick={() => {setModalOpen(!modalOpen)}}
+      >What do I do here?</button>
+      {modalOpen && (
+        <Modal
+        setModalOpen={setModalOpen}
+        />
+      )}
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
       {/* <h1>Song: {data.songs[0].title}</h1> */}
       <form onSubmit={checkAnswer}>
           <input
-            className="border-2 border-indigo-600 p-2"
+            className="rounded-md border-2 border-indigo-600 p-2"
             name="userAnswer"
             value={userAnswer}
             onChange={handleChange}
             placeholder="Enter Song title..."
           />
           <button
-            className="border border-indigo-600 m-2 p-2"
+            className="rounded-md border border-indigo-600 m-2 p-2"
             type="submit"
           >
-            Submit
+            Set the Gary Bar
           </button>
         </form>
+        {isCorrect && (
+          <div>
+          <p>Correct!</p>
+          You have set the Gary Bar low
+          </div>          
+        )}
+        {isIncorrect && (
+          <div>
+          <p>Wrong!</p>
+          You have {livesLeft} wrong answers left.
+          </div>          
+        )}
       </main>
       
     </div>
